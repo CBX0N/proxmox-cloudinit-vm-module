21cb8256d7de3538072467bd282894b321bd5a07
charlie boon <charlie.boon@sky.com>
2024-11-25 17:14:24 +0000

    Splitting vmid

--- variables.tf
+++ variables.tf
@@ -6,10 +6,21 @@ variable "proxmox_ssh_config" {
   })
 }
 
+variable "vmid" {
+  type = number
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "ip_config" {
+  type = string
+}
+
+
 variable "vm_config" {
   type = object({
-    name               = string
-    vmid               = string
     memory             = optional(number, 4096)
     balloon            = optional(number, 4096)
     cores              = optional(number, 4)
@@ -17,7 +28,6 @@ variable "vm_config" {
     disk_size_gb       = optional(number, 40)
     os_type            = optional(string, "cloud-init")
     clone              = string
-    ip_config          = string
     target_node        = optional(string, "pve")
     nic                = optional(string, "virtio")
     bridge             = optional(string, "vmbr0")
@@ -25,6 +35,10 @@ variable "vm_config" {
     cloudinit_location = optional(string, "local-lvm")
     userdata_location  = string
   })
+}
+
+variable "userdata_location" {
+  type = string
 }
 
 variable "userdata_content" {
